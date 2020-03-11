f3fa53a20de9161c387921092a758118ca37520f
ilasso <1204@holbertonschool.com>
2020-03-11 18:11:04 +0000

    task9 betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,15 +14,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t Rheight;
 
 
-	if (tree == NULL ||(tree->left == NULL && tree->right == NULL))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
 		return (0);
+	}
 	else
 	{
-
 		Lheight = binary_tree_height(tree->left) + 1;
 		Rheight = binary_tree_height(tree->right) + 1;
 
-		if ( Lheight > Rheight )
+		if (Lheight > Rheight)
 			return (Lheight);
 		else
 			return (Rheight);
