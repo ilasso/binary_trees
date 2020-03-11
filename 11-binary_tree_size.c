d11b2b5b7599394feee2c61175f3984cd81088b9
ilasso <1204@holbertonschool.com>
2020-03-11 18:18:58 +0000

    task11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,7 +14,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-
 	count = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
 
 	return (count);
