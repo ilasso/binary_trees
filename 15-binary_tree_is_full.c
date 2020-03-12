2c56791fbff55761c63d64e2ae8885aff434da3b
ilasso <1204@holbertonschool.com>
2020-03-12 18:03:49 +0000

    task15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,10 +14,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
+	if ((tree->left == NULL &&
+	    tree->right == NULL) ||
+	    (binary_tree_is_full(tree->left) &&
+	    binary_tree_is_full(tree->right)))
 		return (1);
 
 	return (0);
